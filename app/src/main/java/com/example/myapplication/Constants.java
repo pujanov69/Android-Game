b52ab0d86d73c224a057041c73b4295ac923d815
pujan <pujanov69@gmail.com>
2019-11-21 12:15:44 +0545

    Gyroscopic control added

--- app/src/main/java/com/example/myapplication/Constants.java
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -10,4 +10,6 @@ public class Constants {
     public static int SCREEN_HEIGHT;
 
     public static Context CURRENT_CONTEXT;
+
+    public static long INIT_TIME;
 }
--- app/src/main/java/com/example/myapplication/GamePanel.java
+++ app/src/main/java/com/example/myapplication/GamePanel.java
@@ -44,6 +44,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder){
         thread  = new MainThread(getHolder(), this);
+        Constants.INIT_TIME = System.currentTimeMillis();
         thread.setRunning(true);
         thread.start();
     }
@@ -51,7 +52,7 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
     @Override
     public void surfaceDestroyed(SurfaceHolder holder){
         boolean retry = true;
-        while(true){
+        while(retry){
             try {
                 thread.setRunning(false);
                 thread.join();
